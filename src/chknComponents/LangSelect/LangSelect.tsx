ec1581936b070b31ca8154927240f97197efde68
Baskakova Yana <bask-yana@yandex.ru>
2020-10-17 01:22:44 +0300

    LangSelect

---
+++ src/chknComponents/LangSelect/LangSelect.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import UKFlag from '../../assets/img/united-kingdom.svg'
+import ChinaFlag from '../../assets/img/china.svg'
+import { LangList, LangImage, StyledLang, LangItem, StyledLangSelect } from './styled'
+
+const options = [
+  { img: UKFlag, label: 'English', shortLabel: 'EN' },
+  { img: ChinaFlag, Label: 'Chinese', shortLabel: 'CH'},
+]
+
+const LangSelect = (props) => {
+  const [listVisibility, setListVisibility] = useState(false)
+  const [language, setLanguage] = useState('English')
+
+
+  const onLangClick = () => {
+    setListVisibility(true)
+  }
+
+  const onItemClick = ({label}) => () => {
+    setLanguage
+  }
+
+  const renderLangList = () => {
+    if (!listVisibility) return
+    return options.map((item) => {
+      return (
+        <LangList>
+          <LangItem>
+            <LangImage src={item.img} />
+            <span>{item.label}</span>
+          </LangItem>
+        </LangList>
+      )
+    })
+  }
+
+  return (
+    <StyledLangSelect>
+      <StyledLang onClick={onLangClick}>
+        <LangImage src={UKFlag} />
+        <span>{language}</span>
+      </StyledLang>
+      {renderLangList()}
+    </StyledLangSelect>
+  )
+}
+
+export default LangSelect
---
+++ src/chknComponents/LangSelect/styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const StyledLangSelect = styled.div`
+  display: block;
+`
+
+export const LangList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`
+
+export const LangItem = styled.li`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  
+`
+
+export const LangImage = styled.img`
+  display: inline-block;
+`
+
+export const StyledLang = styled.div`
+  display: block;
+`
